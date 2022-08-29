906aa0eb0b53c2585a8a006ede21838ece577d90
Eric-Garcia-Huitzitl <eric.gahu20@gmail.com>
2022-08-29 12:30:45 -0500

    Preprocessing directory add

--- Analysis/dataAnalysis.py
+++ Analysis/dataAnalysis.py
@@ -1,6 +1,7 @@
 #Read CSV 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 nfile = 'TrainDos.csv'
 namesX = ['id','duration','src_bytes','dst_bytes','land','wrong_fragment','urgent','hot','num_failed_logins','logged_in','num_compromised','root_shell','su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds','is_host_login','is_guest_login','count','srv_count','serror_rate','srv_serror_rate','rerror_rate,srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count','dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate','dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate','dst_host_srv_rerror_rate','protocol_type','service','flag','xAttack']
@@ -23,15 +24,6 @@ print("Correlation Matrix")
 print(df.corr())
 dCor = df.corr()
 
-
-
-
-
-
-
-
-
-
-
-
+df.hist()
+plt.show()
 
